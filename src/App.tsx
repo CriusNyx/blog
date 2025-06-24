82f73b6990913d3e423411441632cd81c1409b6c
CriusNyx <rjreynoldsw@gmail.com>
2025-06-23 21:49:54 -0400

    history patch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { routeTree } from "./routeTree.gen.ts";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import {
+  createMemoryHistory,
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
 import { MDXProvider } from "@mdx-js/react";
 import "./prism.css";
 import { MDXComponents } from "./MDXComponents/index.tsx";
 
-const router = createRouter({ routeTree });
+const memoryHistory = createMemoryHistory({
+  initialEntries: ["/"],
+});
+
+const router = createRouter({ routeTree, history: memoryHistory });
 
 function App() {
   return (
