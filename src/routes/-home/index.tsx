d558de14ffdee854d5ff375801feab6bef51e8ac
CriusNyx <rjreynoldsw@gmail.com>
2025-06-23 21:52:21 -0400

    routing fix

--- src/routes/-home/index.tsx
+++ src/routes/-home/index.tsx
@@ -13,6 +13,9 @@ export function Home() {
       <Link to="/blog/doingCoolThingsWithMarkdown2/">
         <h4>Doing Cool Things (with Markdown 2)</h4>
       </Link>
+      <Link to="/blog/jeremy/">
+        <h4>Jeremy</h4>
+      </Link>
     </div>
   );
 }
