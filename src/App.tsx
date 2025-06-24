a833244260d64c61ed9cbe70b771205c622c0049
CriusNyx <rjreynoldsw@gmail.com>
2025-06-23 21:54:44 -0400

    Changed to hash routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { routeTree } from "./routeTree.gen.ts";
-import {
-  createMemoryHistory,
-  createRouter,
-  RouterProvider,
-} from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { MDXProvider } from "@mdx-js/react";
 import "./prism.css";
 import { MDXComponents } from "./MDXComponents/index.tsx";
+import { createHashHistory } from "@tanstack/react-router";
 
-const memoryHistory = createMemoryHistory({
-  initialEntries: ["/"],
-});
+const memoryHistory = createHashHistory();
 
 const router = createRouter({ routeTree, history: memoryHistory });
 
